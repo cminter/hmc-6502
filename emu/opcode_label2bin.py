45ea7bb60c6a0c15562fcadfdb8e17b2ac16aed5
Kyle.L.Marsh <Kyle.L.Marsh@3811012a-133b-0410-87d1-4b30988b6512>
2008-03-05 05:03:19 +0000

    Primary testing of opcode_label2bin.py does not work -- will look into this further later tonight.

--- emu/opcode_label2bin.py
+++ emu/opcode_label2bin.py
@@ -11,11 +11,11 @@
 #       *tranlsated_opcodes.txt # List of lines for control.sv with
 #                               # next-state numbers in place
 #
-#   THIS CODE HAS NOT BEEN TESTED: I'VE TESTED ALL THE IMPORTANT BITS, BUT
-#   WE MAY STILL NEED TO DEBUG THE FILEIO STUFF.
+#   THIS CODE DOES NOT YET WORK.  DO NOT DEPEND ON IT YET
 
-help = """Translation script to replace next-state names with their
-corresponding numbers as defined in 6502.ucode.compiled.
+help = """opcode_label2bin:
+    Translation script to replace next-state names with their corresponding
+    numbers as defined in 6502.ucode.compiled.
 """
 
 import re
@@ -35,15 +35,34 @@ def main():
 
     # Open the input file in a semi-robust manner.
     try:
-        infile = open(sys.argv[1])
+        infilename = sys.argv[1]
     except IndexError:
         usage()
-        sys.stderr.write('Please provide the input filename.\n')
+        sys.stderr.write('Please provide the input filename as the first\
+                argument on the command line.\n')
+        sys.exit(1)
+
+    # HACK: Debug mode turned on when command line contains anything after
+    # the input file name.
+    try:
+        d = sys.argv[2]
+        debug = True
+    except IndexError:
+        debug = False
+
+    try:
+        if debug: sys.stderr.write('Opening file %s...' %infilename)
+        infile = open(infilename)
+    except IOError:
+        usage()
+        sys.stderr.write('Provided filename not valid.\n')
         sys.exit(1)
     
+    if debug: sys.stderr.write('succeeded!\n')
     # Hard-coded relative path to ucode file
     namefile = open('../src/ucode/6502.ucode.compiled')
     name_list = namefile.readlines()
+    
     # Magic list comprehension to suck out the lines with the labels and
     # numbers.  First chop off the leading comments which get in the way.
     name_list = name_list[4:]
@@ -55,18 +74,19 @@ def main():
     name_dict = {}
     for line in name_list:
         line = line.split(':')
-        name_dict[line[0]] = int2bin(line[1], NUMBITS)
+        name_dict[line[1]] = int2bin(int(line[1]), NUMBITS)
 
     # Read and parse the input list.  Uses an even more magic list
     # comprehension that matches one of the values in name_dict against each
     # line and replaces it with its corresponding value
     input_list = infile.readlines()
-    output_list = [re.sub(pat, dict[pat], line) for line in input_list for
-            pat in name_dict if re.search(pat, line)]
+    output_list = [re.sub(pat, name_dict[pat], line) for line in input_list
+            for pat in name_dict if re.search(pat, line)]
 
     # Write out the new file.
     outfile = open('translated_opcodes.txt', 'w')
-    outfile.write(output_list)
+    output_string = ''.join(output_list)
+    outfile.write(output_string)
     outfile.close()
 
 

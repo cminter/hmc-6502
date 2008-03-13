1e15fb7d8a739a74f23becc4776b537d8224382a
twbarr <twbarr@3811012a-133b-0410-87d1-4b30988b6512>
2008-03-13 08:11:07 +0000

    changed ALU ordering to help out ALU team.

--- src/alu.sv
+++ src/alu.sv
@@ -20,17 +20,17 @@ module alu(input logic [7:0] a, b,
   
   always_comb begin
     case (op)
-      4'h0: {c_out, y} = a + b + c_in; // add
-      4'h1: {c_out, y} = b - a - c_in; // sub.
-      4'h2: {c_out, y} = {1'b0, a | b}; // OR
-      4'h3: {c_out, y} = {1'b0, a & b}; // AND
-      4'h4: {c_out, y} = {1'b0, a ^ b}; // EOR
-      4'h5: {c_out, y} = a + c_in; // inc
-      4'h6: {c_out, y} = a - c_in; // dec
-      4'h7: {c_out, y} = {a, 1'b0}; // asl
-      4'h8: {c_out, y} = {a, c_in}; // rol
-      4'h9: {y, c_out} = {c_in, a}; // ror
-      4'ha: {y, c_out} = {1'b0, a & b}; // test bits
+      4'h0: {c_out, y} = a + c_in; // inc
+      4'h1: {c_out, y} = a - c_in; // dec
+      4'h2: {c_out, y} = a + b + c_in; // add
+      4'h3: {c_out, y} = b - a - c_in; // sub.
+      4'h4: {c_out, y} = {1'b0, a | b}; // OR
+      4'h5: {c_out, y} = {a, 1'b0}; // asl
+      4'h6: {c_out, y} = {a, c_in}; // rol
+      4'h7: {y, c_out} = {c_in, a}; // ror
+      4'h8: {c_out, y} = {1'b0, a & b}; // AND
+      4'h9: {y, c_out} = {1'b0, a & b}; // test bits
+      4'ha: {c_out, y} = {1'b0, a ^ b}; // EOR
       4'hb: {y, c_out} = 9'b111111111; // ones (for setting flags)
       default: {y, c_out} = 9'b0;
     endcase

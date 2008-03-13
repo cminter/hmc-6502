747aaf8701527268789accdde1a6b48aa14488db
HJustice@hmc.edu <HJustice@hmc.edu@3811012a-133b-0410-87d1-4b30988b6512>
2008-03-13 07:06:31 +0000

    hoping that my svn is no longer broken...

---
+++ emu/testvectors/TestAllInstructions/test02-incdec.asm
@@ -0,0 +1,45 @@
+	.ORG $600
+
+start:
+	LDA #$FF
+	LDX #$00
+	
+	STA $90
+	INC $90
+	INC $90
+	LDA $90
+	LDX $90
+	
+	STA $90,X
+	INC $90,X
+	LDA $90,X
+	LDX $91
+	
+	STA $0190,X
+	INC $0192
+	LDA $0190,X
+	LDX $0192
+	
+	STA $0190,X
+	INC $0190,X
+	LDA $0190,X
+	LDX $0193
+	
+	STA $0170,X
+	DEC $0170,X
+	LDA $0170,X
+	LDX $0174
+	
+	STA $0170,X
+	DEC $0173
+	LDA $0170,X
+	LDX $0173
+
+	STA $70,X
+	DEC $70,X
+	LDA $70,X
+	LDX $72
+	
+	STA $70,X
+	DEC $71
+	DEC $71

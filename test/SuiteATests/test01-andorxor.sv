4740c798a871ead24115e08770b6aa1eaea65ec4
HJustice@hmc.edu <HJustice@hmc.edu@3811012a-133b-0410-87d1-4b30988b6512>
2008-03-25 08:52:56 +0000

    working on making test suites function

---
+++ test/SuiteATests/test01-andorxor.sv
@@ -0,0 +1,51 @@
+// test01-andorxor.sv
+// basic regression test
+// tbarr at cs dot hmc dot edu
+
+`timescale 1 ns / 1 ps
+
+module optest;
+  reg ph1, ph2, reset;
+  
+  wire [7:0] data;
+  
+  top top(ph1, ph2, reset);
+  
+  always begin
+    ph1 <= 1; #8; ph1 <= 0; #12;
+  end
+  always begin
+    ph2 <= 0; #10; ph2 <= 1; #8; ph2 <= 0; #2;
+  end
+  
+  initial begin
+    // init ROM
+    top.mem.ROM[4093] = 8'hf0;
+    top.mem.ROM[4092] = 8'h00;
+    
+    top.chip.core.dp.regfile.reg_file[0] = 8'h0;
+    top.chip.core.dp.regfile.reg_file[1] = 8'h0;
+    top.chip.core.dp.regfile.reg_file[2] = 8'h0;
+    top.chip.core.dp.regfile.reg_file[3] = 8'h0;
+    
+    top.chip.core.dp.flaglatch.latch0.q = 0;
+    top.chip.core.dp.flaglatch.latch1.q = 0;
+    top.chip.core.dp.flaglatch.latch2.q = 0;
+    top.chip.core.dp.flaglatch.latch3.q = 0;
+    top.chip.core.dp.flaglatch.latch4.q = 0;
+    top.chip.core.dp.flaglatch.latch5.q = 0;
+    top.chip.core.dp.flaglatch.latch6.q = 0;
+    top.chip.core.dp.flaglatch.latch7.q = 0;
+    
+    // path relative to this file.
+    $readmemh("test/roms/SuiteA/test01-andorxor.rom", top.mem.ROM);
+    
+    // start test
+    reset = 1;
+    #100;
+    reset = 0;
+    #500;
+    assert (top.mem.RAM[169] == 8'hAA) $display ("PASSED Test 01 - and & or & xor");
+      else $error("FAILED Test 01 - and & or & xor");
+  end
+endmodule
---
+++ test/unittests/PowerTest.sv
@@ -0,0 +1,46 @@
+// PowerTest.sv
+// basic regression test
+// tbarr at cs dot hmc dot edu
+
+`timescale 1 ns / 1 ps
+
+module optest;
+  reg ph1, ph2, reset;
+  
+  wire [7:0] data;
+  
+  top top(ph1, ph2, reset);
+  
+  always begin
+    ph1 <= 1; #8; ph1 <= 0; #12;
+  end
+  always begin
+    ph2 <= 0; #10; ph2 <= 1; #8; ph2 <= 0; #2;
+  end
+  
+  initial begin
+    // init ROM
+    top.mem.ROM[4093] = 8'hf0;
+    top.mem.ROM[4092] = 8'h00;
+    
+    top.chip.core.dp.flaglatch.latch0.q = 0;
+    top.chip.core.dp.flaglatch.latch1.q = 0;
+    top.chip.core.dp.flaglatch.latch2.q = 0;
+    top.chip.core.dp.flaglatch.latch3.q = 0;
+    top.chip.core.dp.flaglatch.latch4.q = 0;
+    top.chip.core.dp.flaglatch.latch5.q = 0;
+    top.chip.core.dp.flaglatch.latch6.q = 0;
+    top.chip.core.dp.flaglatch.latch7.q = 0;
+    
+    // path relative to this file.
+    $readmemh("test/roms/PowerTest.rom", top.mem.ROM);
+    
+    // start test
+    reset = 1;
+    #100;
+    reset = 0;
+    #500;
+    assert (top.mem.RAM[66] == 8'hCF) $display ("PASSED Power Test");
+      else $error("FAILED Power Test");
+  end
+endmodule

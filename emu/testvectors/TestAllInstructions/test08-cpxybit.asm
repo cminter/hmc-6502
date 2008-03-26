7c55e4be036501094fd188792e878fb24b05426d
HJustice@hmc.edu <HJustice@hmc.edu@3811012a-133b-0410-87d1-4b30988b6512>
2008-03-26 05:52:23 +0000

    more suite A microcode

---
+++ emu/testvectors/TestAllInstructions/test08-cpxybit.asm
@@ -0,0 +1,58 @@
+	.ORG 600
+start:
+	; prepare memory
+	LDA #$A5
+	STA $20
+	STA $0120
+	LDA #$5A
+	STA $21
+	
+	; cpx imm...
+	LDX #$A5
+	CPX #$A5
+	BEQ b1 ; taken
+	LDX #$01 ; not done
+b1:
+	; cpx zpg...
+	CPX $20
+	BEQ b2 ; taken
+	LDX #$02 ; not done
+b2:
+	; cpx abs...
+	CPX $0120
+	BEQ b3 ; taken
+	LDX #$03 ; not done
+b3:
+	; cpy imm...
+	STX $30
+	LDY $30
+	CPY #$A5
+	BEQ b4 ; taken
+	LDY #$04 ; not done
+b4:
+	; cpy zpg...
+	CPY $20
+	BEQ b5 ; taken
+	LDY #$05 ; not done
+b5:
+	; cpy abs...
+	CPY $0120
+	BEQ b6 ; taken
+	LDY #$06 ; not done
+b6:	
+	; bit zpg...
+	STY $31
+	LDA $31
+	BIT $20
+	BNE b7 ; taken
+	LDA #$07 ; not done
+b7:
+	; bit abs...
+	BIT $0120
+	BNE b8 ; taken
+	LDA #$08 ; not done
+b8:
+	BIT $21
+	BNE b9 ; not taken
+	STA $42	
+b9:

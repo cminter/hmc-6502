fb823a9b0ea9ff42ad0da9ea3c6c82f9327479e8
Kyle.L.Marsh <Kyle.L.Marsh@3811012a-133b-0410-87d1-4b30988b6512>
2008-04-03 07:12:07 +0000

    Fixed subtraction in alu.sv -- it was b-a-c_in but should have been b-a-~c_in.

--- src/alu.sv
+++ src/alu.sv
@@ -23,7 +23,7 @@ module alu(input logic [7:0] a, b,
       4'h0: {c_out, y} = a + c_in; // inc
       4'h1: {c_out, y} = a - c_in; // dec
       4'h2: {c_out, y} = a + b + c_in; // add
-      4'h3: {c_out, y} = b - a - c_in; // sub.
+      4'h3: {c_out, y} = b - a - ~c_in; // sub.
       4'h4: {c_out, y} = {1'b0, a | b}; // OR
       4'h5: {c_out, y} = {a, 1'b0}; // asl
       4'h6: {c_out, y} = {a, c_in}; // rol

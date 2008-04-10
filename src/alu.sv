37f762c6917d2f8b1c4865d7012c65d784b3f85b
HJustice@hmc.edu <HJustice@hmc.edu@3811012a-133b-0410-87d1-4b30988b6512>
2008-04-10 04:16:23 +0000

    alu change...

--- src/alu.sv
+++ src/alu.sv
@@ -22,12 +22,28 @@ module alu(input logic [7:0] a, b,
   logic sub_c_out;
   adderc #8 sub_adder(~a, b, c_in, sub_y, sub_c_out);
 
+  logic [7:0] adc_sum;
+  logic [6:0] adc_low7_sum;
+  logic adc_low7_cout, adc_high_sum, adc_cout;
+  adderc #7 adc_lower7_add(a[6:0], b[6:0], c_in, adc_low7_sum, adc_low7_cout);
+  adderc #1 adc_high_add(a[7], b[7], adc_low7_cout, adc_high_sum, adc_cout);
+  assign adc_sum = {adc_high_sum, adc_low7_sum};
+
+  logic [7:0] sbc_sum;
+  logic [6:0] sbc_low7_sum;
+  logic sbc_low7_cout, sbc_high_sum, sbc_cout;
+  adderc #7 sbc_lower7_add(~a[6:0], b[6:0], c_in, sbc_low7_sum, sbc_low7_cout);
+  adderc #1 sbc_high_add(~a[7], b[7], sbc_low7_cout, sbc_high_sum, sbc_cout);
+  assign sbc_sum = {sbc_high_sum, sbc_low7_sum};
+
+  logic carry6, carry7;
+
   always_comb begin
     case (op)
       4'h0: {c_out, y} = a + c_in; // inc
       4'h1: {c_out, y} = a - c_in; // dec
-      4'h2: {c_out, y} = a + b + c_in; // add
-      4'h3: {c_out, y} = {sub_c_out, sub_y}; // sub
+      4'h2: {c_out, y} = {adc_cout, adc_sum}; // add
+      4'h3: {c_out, y} = {sbc_cout, sbc_sum}; // sub
       4'h4: {y, c_out} = {c_in, a}; // ror
       4'h5: {c_out, y} = {a, 1'b0}; // asl
       4'h6: {c_out, y} = {a, c_in}; // rol

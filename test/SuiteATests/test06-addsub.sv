d1d7700b93677c8d3125c098843efe38f0bddd24
Kyle.L.Marsh <Kyle.L.Marsh@3811012a-133b-0410-87d1-4b30988b6512>
2008-04-19 23:41:21 +0000

    Removed flag initialization from test06-addsub.sv.

--- test/SuiteATests/test06-addsub.sv
+++ test/SuiteATests/test06-addsub.sv
@@ -27,15 +27,6 @@ module optest;
     top.mem.ROM[4093] = 8'hf0;
     top.mem.ROM[4092] = 8'h00;
     
-    top.chip.core.dp.flaglatch.latch0.q = 0;
-    top.chip.core.dp.flaglatch.latch1.q = 0;
-    top.chip.core.dp.flaglatch.latch2.q = 0;
-    top.chip.core.dp.flaglatch.latch3.q = 0;
-    top.chip.core.dp.flaglatch.latch4.q = 0;
-    top.chip.core.dp.flaglatch.latch5.q = 0;
-    top.chip.core.dp.flaglatch.latch6.q = 0;
-    top.chip.core.dp.flaglatch.latch7.q = 0;
-    
     // path relative to this file.
     $readmemh("test/roms/SuiteA/test06-addsub.rom", top.mem.ROM);
     

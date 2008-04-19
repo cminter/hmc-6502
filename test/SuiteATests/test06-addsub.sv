d198b25e2f4ed81d1233a4ebc28cd5ab234eb9fe
Kyle.L.Marsh <Kyle.L.Marsh@3811012a-133b-0410-87d1-4b30988b6512>
2008-04-19 23:39:44 +0000

    Removed regfile initialization from test06-addsub.sv.

--- test/SuiteATests/test06-addsub.sv
+++ test/SuiteATests/test06-addsub.sv
@@ -27,11 +27,6 @@ module optest;
     top.mem.ROM[4093] = 8'hf0;
     top.mem.ROM[4092] = 8'h00;
     
-    top.chip.core.dp.regfile.reg_file[0] = 8'h0;
-    top.chip.core.dp.regfile.reg_file[1] = 8'h0;
-    top.chip.core.dp.regfile.reg_file[2] = 8'h0;
-    top.chip.core.dp.regfile.reg_file[3] = 8'h0;
-    
     top.chip.core.dp.flaglatch.latch0.q = 0;
     top.chip.core.dp.flaglatch.latch1.q = 0;
     top.chip.core.dp.flaglatch.latch2.q = 0;
